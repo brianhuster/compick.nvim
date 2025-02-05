324ad0ce51429915cc4f94cf28cf8e6d1eb7d9f5
brianhuster <phambinhanctb2004@gmail.com>
2025-02-05 16:01:27 +0700

    update

--- lua/compick/grep.lua
+++ lua/compick/grep.lua
@@ -5,20 +5,25 @@ M.cache = {}
 function M.pick(initial_text)
 	M.cache = {}
 	require('compick').pick(function(base)
-		if not base or base == '' then
-			return
+		if not base or base == "" then
+			return {}
 		end
 		if M.cache[base] then
 			return M.cache[base]
 		end
 		vim.system(vim.list_extend(vim.split(vim.o.grepprg:sub(1, -2), ' '), { base }), {
 			text = true,
-		}, function(obj)
-			if obj.stdout then
-				M.cache[base] = vim.split(obj.stdout, '\n')
-				vim.schedule(require('compick._').trigger_compl)
+			stdout = function(_, data)
+				if data then
+					local data_list = vim.split(data, '\n')
+					M.cache[base][#M.cache[base]] = M.cache[base][#M.cache[base]] .. data_list[1]
+					for k = 2, #data_list do
+						M.cache[base][#M.cache[base] + 1] = data_list[k]
+					end
+				end
+				require('compick._').trigger_compl()
 			end
-		end)
+		})
 	end, function(selected)
 		local item = vim.fn.getqflist({ lines = { selected }, efm = vim.o.grepformat }).items[1]
 		if item.bufnr > 0 then
